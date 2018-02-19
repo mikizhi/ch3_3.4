6f6b2d7bed6de964ee98e43a4611adef320097f4
mikizhi <mikizhi@gmail.com>
2018-02-19 16:06:08 -0500

    Add files via upload

---
+++ exercise3_4.java
@@ -0,0 +1,35 @@
+public class ch3_3dot4 {
+
+	public static void main(String[] args) {
+		
+		
+		int rdmNumber = (int)(Math.random()*12)+1;
+		
+		if (rdmNumber == 1)
+			System.out.println("January");
+		else if (rdmNumber == 2)
+			System.out.println("February");
+		else if (rdmNumber == 3)
+			System.out.println("March");
+		else if (rdmNumber == 4)
+			System.out.println("April");
+		else if (rdmNumber == 5)
+			System.out.println("May");
+		else if (rdmNumber == 6)
+			System.out.println("June");
+		else if (rdmNumber == 7)
+			System.out.println("July");
+		else if (rdmNumber == 8)
+			System.out.println("August");
+		else if (rdmNumber == 9)
+			System.out.println("September");
+		else if (rdmNumber == 10)
+			System.out.println("October");
+		else if (rdmNumber == 11)
+			System.out.println("November");
+		else if (rdmNumber == 12)
+			System.out.println("December");
+
+	}
+
+}
